bf4711178cdaf353d82e56c2dfeb8c5d27ba587c
Lee Reyes <reyeslee94@gmail.com>
2023-02-14 22:20:26 -0800

    adding tags

--- src/main/java/com/adacapstone/bug/tracker/api/Bug/BugRepository.java
+++ src/main/java/com/adacapstone/bug/tracker/api/Bug/BugRepository.java
@@ -1,9 +1,11 @@
 package com.adacapstone.bug.tracker.api.Bug;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BugRepository extends JpaRepository<Bug, Long> {
     Bug findByBugId(Long Id);
 //    List<Bug> findBugsByUserId(Long userId);
--- src/main/java/com/adacapstone/bug/tracker/api/User/UserRepository.java
+++ src/main/java/com/adacapstone/bug/tracker/api/User/UserRepository.java
@@ -1,10 +1,12 @@
 package com.adacapstone.bug.tracker.api.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     Optional<User> findByEmail(String email);
 //    List<User> findUsersByBugId(Long bugId);
